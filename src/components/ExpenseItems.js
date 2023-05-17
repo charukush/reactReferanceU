1bcf7fb552fc7131c46e44d8d48591afe33e228a
Charu Kush <charu.kush@gmail.com>
2023-05-17 12:31:09 -0700

    Updated date format

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,13 +1,20 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItems(props) {
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__description'>
-              <h2 className='expense-item h2'>{props.title }</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
