4bf93627941fb2851ba4198c4ec0bfc7c1b774fd
Charu Kush <charu.kush@gmail.com>
2023-05-17 14:56:47 -0700

    Added code for assigment to split expenses list items component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import ExpenseItems from "./components/ExpenseItems";
-
-function App() {
+import Expenses from "./components/Expenses";
+function App(props) {
   const expenses = [
     {
       id: "e1",
@@ -25,26 +24,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItems
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItems
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItems
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItems
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses expenses={expenses } />
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,5 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+
 function ExpenseItems(props) {
   return (
     <div className="expense-item">
